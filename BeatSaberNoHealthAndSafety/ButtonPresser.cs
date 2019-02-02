489ec1bf16c427d198365af68f36b5659fa38647
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2019-02-02 05:08:46 -0800

    initial commit. seems working

---
+++ BeatSaberNoHealthAndSafety/ButtonPresser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatSaberNoHealthAndSafety
+{
+    class ButtonPresser : MonoBehaviour
+    {
+        private IEnumerator clickIt()
+        {
+            yield return new WaitForSeconds(.3f);
+            foreach (Button button in Resources.FindObjectsOfTypeAll<Button>())
+            {
+                if (button.name == "Continue")
+                {
+                    Console.WriteLine("[NoHealthAndSafety]: Clicking 'continue' through the safety warning");
+                    button.onClick.Invoke();
+                }
+            }
+        }
+
+        void Awake()
+        {
+            StartCoroutine(clickIt());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# BeatSaberNoHealthAndSafety
+A plugin to skip the health and safety warning at the beginning of the game.
+
+It works by automatically pressing the "Continue" button after a short delay.
+
+Do not use this mod if you need the warning in order to stay healthy and safe.
+
+Tested on v0.12.2
+
+
